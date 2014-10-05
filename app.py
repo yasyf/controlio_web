bd97faaa79ce54eead1b49faaf73650e2b2eb96b
Yasyf Mohamedali <yasyfm@gmail.com>
2014-10-05 00:28:23 -0400

    Add insert and send routes

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify, request, session, redirect, url_for, render_template, flash, g
 from setup import *
-import os, bcrypt, uuid
+import os, bcrypt, uuid, datetime
 from bson.objectid import ObjectId
 import twilio.twiml
+from twilio.rest import TwilioRestClient
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SK')
@@ -39,8 +40,20 @@ def login_signup_view():
 @app.route('/insert', methods=['POST', 'GET'])
 def insert_view():
   resp = twilio.twiml.Response()
-  resp.message("It workd!")
+  from_number = request.values.get('From')
+  user = users.find_one({'number': from_number})
+  if not from_number or not user:
+    resp.message("We couldn't find that number in our database!")
+  else:
+    pending_commands.insert({'key': user['key'], 'message': request.values.get('Body'), 'datetime': datetime.datetime.utcnow()})
   return str(resp)
+
+@app.route('/send', methods=['POST', 'GET'])
+def send_view():
+  client = TwilioRestClient(account=os.getenv('TWILIO_ACCOUNT_SID'), token=os.getenv('TWILIO_AUTH_TOKEN'))
+  user = users.find_one({'key': uuid.UUID(request.values.get('key'))})
+  message = client.messages.create(to="+1"+user['number'], from_="+1"+os.getenv('TWILIO_NUMBER'), body=request.values.get('message'))
+  return str(message)
 
 if __name__ == '__main__':
   if os.environ.get('PORT'):
