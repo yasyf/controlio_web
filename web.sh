f30dd39cb413b6d1dc77c0857d451e5d5db0b1e1
Yasyf Mohamedali <yasyfm@gmail.com>
2014-10-05 07:11:07 -0400

    Fix dev mode

--- web.sh
+++ web.sh
@@ -1,7 +1,7 @@
 chmod a+x /app/.heroku/python/lib/python2.7/site-packages/speech_recognition/flac-linux-i386
-if [[ "$DEV" -eq "true" ]]
+if [ "$DEV" = "true" ]
 then
-  gunicorn -b "0.0.0.0:$PORT" app:app
-else
   python app.py
+else
+  gunicorn -b "0.0.0.0:$PORT" app:app
 fi
