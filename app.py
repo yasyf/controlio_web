6c97b4caab0ce1a659b8fdfc9fca5a1b27547252
Yasyf Mohamedali <yasyfm@gmail.com>
2014-10-05 01:33:10 -0400

    poll and destroy views

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def login_signup_view():
 def insert_view():
   resp = twilio.twiml.Response()
   from_number = request.values.get('From')
-  user = users.find_one({'number': from_number})
+  user = users.find_one({'number': from_number[2:]})
   if not from_number or not user:
     resp.message("We couldn't find that number in our database!")
   else:
@@ -54,6 +54,18 @@ def send_view():
   user = users.find_one({'key': uuid.UUID(request.values.get('key'))})
   message = client.messages.create(to="+1"+user['number'], from_="+1"+os.getenv('TWILIO_NUMBER'), body=request.values.get('message'))
   return str(message)
+
+@app.route('/poll', methods=['POST', 'GET'])
+def poll_view():
+  commands = list(pending_commands.find({'key': uuid.UUID(request.values.get('key'))}))
+  for command in commands:
+    command['_id'] = str(command['_id'])
+  return jsonify({'commands': commands})
+
+@app.route('/destroy/<object_id>', methods=['POST', 'GET'])
+def destroy_view(object_id):
+  response = pending_commands.remove({'_id': ObjectId(object_id), 'key': uuid.UUID(request.values.get('key'))})
+  return jsonify({'response': response})
 
 if __name__ == '__main__':
   if os.environ.get('PORT'):
